4bd8733d320883ef5667c1a57009af5e9c1565d0
Viva <happybird00@naver.com>
2021-04-12 09:49:55 +0900

    Initial commit

---
+++ src/main/java/com/example/find_dog/FindDogApplication.java
@@ -0,0 +1,13 @@
+package com.example.find_dog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindDogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindDogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/find_dog/FindDogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.find_dog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindDogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
